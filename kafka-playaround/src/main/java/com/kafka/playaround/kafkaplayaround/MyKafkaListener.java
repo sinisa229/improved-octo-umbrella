7784fcf0b33b065882a7c94424eccf03eee3d0ea
SMihajlovski <sinisha.mihajlovski@esure.com>
2021-09-30 14:38:05 +0100

    Added docker compose; Updated spring-version.

--- kafka-playaround/src/main/java/com/kafka/playaround/kafkaplayaround/MyKafkaListener.java
+++ kafka-playaround/src/main/java/com/kafka/playaround/kafkaplayaround/MyKafkaListener.java
@@ -3,8 +3,6 @@ package com.kafka.playaround.kafkaplayaround;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
@@ -13,13 +11,9 @@ public class MyKafkaListener {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(MyKafkaListener.class);
 
-    @Autowired
-    private MySender sender;
-
     @KafkaListener(topics = "myTopic")
-    public void listen(ConsumerRecord<?, ?> cr) throws Exception {
+    public void listen(ConsumerRecord<?, ?> cr) {
         LOGGER.info(cr.toString());
-        sender.countDown();
     }
 
 }
--- kafka-playaround/src/main/java/com/kafka/playaround/kafkaplayaround/MySender.java
+++ kafka-playaround/src/main/java/com/kafka/playaround/kafkaplayaround/MySender.java
@@ -7,8 +7,7 @@ import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.TimeUnit;
+import java.util.Date;
 
 @Component
 public class MySender {
@@ -18,20 +17,10 @@ public class MySender {
     @Autowired
     private KafkaTemplate<String, String> template;
 
-    private final CountDownLatch latch = new CountDownLatch(3);
-
     @Scheduled(fixedDelay = 5000, initialDelay = 1000)
     public void run() throws Exception {
         LOGGER.info("Sending");
-        this.template.send("myTopic", "foo1");
-        this.template.send("myTopic", "foo2");
-        this.template.send("myTopic", "foo3");
-        latch.await(60, TimeUnit.SECONDS);
-        LOGGER.info("All received");
-    }
-
-    public void countDown() {
-        latch.countDown();
+        this.template.send("myTopic", "foo1"+ new Date());
     }
 
 }
